7fb94a8a66a3e636cdeb27f4b7fcb45880505c47
Simon Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2024-06-13 10:38:09 +0200

    feat(auth): add initial setup for Supabase authentication

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@/utils/supabase/server";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+  const origin = requestUrl.origin;
+
+  if (code) {
+    const supabase = createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(`${origin}/dashboard`);
+}
